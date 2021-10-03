c6cb3c77c25ace434412ba73d076d056794dfbfc
zZMiltonZz <91810223+zZMiltonZz@users.noreply.github.com>
2021-10-03 11:57:23 +0200

    Add files via upload

---
+++ GFX Pack/Gfx Sites.bat
@@ -0,0 +1,7 @@
+%ECHO OFF
+SET BROWSER=chrome.exe
+SET WAIT_TIME=2
+START %BROWSER% -new-tab "https://www.befunky.com/features/blur-image/"
+START %BROWSER% -new-tab "https://www.remove.bg/"
+START %BROWSER% -new-tab "https://pixlr.com/se/e/"
+pause
